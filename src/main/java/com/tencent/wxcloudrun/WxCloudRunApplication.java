0896bf9ec12356308fd0d8cda15e7058b0136e20
facedawn <1637537024@qq.com>
2022-01-19 03:00:32 +0800

    ...

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -3,10 +3,12 @@ package com.tencent.wxcloudrun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.tencent.wxcloudrun.*"})
+@EnableConfigurationProperties
 @MapperScan(basePackages = {"com.tencent.wxcloudrun.dao","com.tencent.wxcloudrun.mapper"})
-public class WxCloudRunApplication {  
+public class WxCloudRunApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WxCloudRunApplication.class, args);
